079e4881af44a35f73991eeca5d2d8e22edfc614
7sreekutty <bnairaiswarya@gmail.com>
2019-03-12 13:47:35 +0530

    hai

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+class Constructor{
+    int x;
+    Constructor(){
+System.out.println("Constructor called");
+x=100;
+    }
+    public static void main(String[] args) {
+    Constructor ob=new Constructor();
+    System.out.println();
+}
+    
+ 
+} 
