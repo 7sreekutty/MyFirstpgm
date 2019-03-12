ead0b513d6db7857986a9e4aab31c88f2e9559b9
7sreekutty <bnairaiswarya@gmail.com>
2019-03-12 11:01:58 +0530

    hello

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Add{
+   public static void main(String[] args) {
+       Scanner myinput=new Scanner(System.in);
+       int a,b,c;
+       System.out.println("enter the first number");
+       a=myinput.nextInt();
+       System.out.println("enter the second number");
+       b=myinput.nextInt();
+       c=a+b;
+       System.out.println(c);
+   }
+        
+    
+}
